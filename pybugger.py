e52f830d171e1964302849b37efa9d0bd7be1fa4
= <=>
2015-12-12 23:20:49 -0500

    Inserting initial code into pybugger and debugging

--- pybugger.py
+++ pybugger.py
@@ -14,3 +14,24 @@
 #   of failure in the code.
 # Input: name of C/C++ code file
 # Output: C/C++ code file with debugging printf statements
+
+import sys
+import string
+
+# open and read lines of code file
+inf = open(sys.argv[1])
+lines = inf.readlines()
+inf.close()
+
+# pre-process file
+for i in xrange(len(lines)):
+    lines[i] = lines[i].strip('\n').split('{')
+
+# generate identifiers
+alphabet = string.ascii_lowercase
+
+# insert printf statements after '{'
+for i in xrange(len(lines)):
+    if len(lines[i]) > 2:
+        # TODO insert printf
+        pass
